3848e2954ae896f1cf301e4e6686a36e97457217
Lavesh1208 <laveshs1282@gmail.com>
2023-09-18 16:15:47 +0530

    User Header

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
 import { extendTheme, ThemeConfig, ColorModeScript } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
 const styles = {
 	global: (props) => ({
@@ -31,9 +32,11 @@ const theme = extendTheme({ config, styles, colors });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<ChakraProvider theme={theme}>
-			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
-			<App />
-		</ChakraProvider>
+		<BrowserRouter>
+			<ChakraProvider theme={theme}>
+				<ColorModeScript initialColorMode={theme.config.initialColorMode} />
+				<App />
+			</ChakraProvider>
+		</BrowserRouter>
 	</React.StrictMode>
 );
